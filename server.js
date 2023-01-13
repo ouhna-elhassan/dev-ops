6f28860ae244dd7d34f7516f2fee92b8fcf23170
ouhna-elhassan <ouhnahassan01@gmail.com>
2023-01-13 09:59:48 +0100

    first project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+var corsOptions = {
+    origin : "http://localhost:8081"
+}
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//hello
+app.get("/", (req, res) =>{
+    res.json({message:"je suis ouhna elhassan"})
+})
+
+app.listen(8080, () =>{
+    console.log("server listening on port 8080")
+})
